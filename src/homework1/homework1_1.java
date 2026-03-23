a4aceb9748ac5aa1ac57660108088296b6883375
Egor Permyakov <wot0110@yandex.ru>
2026-03-23 17:50:48 +0300

    Домашнее задание 1

---
+++ src/homework1/homework1_1.java
@@ -0,0 +1,10 @@
+package homework1;
+
+public class homework1_1 {
+    public static void main(String[] args) {
+        int b = 22;
+        int c = 33;
+        int a = 4 * (b + c - 1) / 2;
+        System.out.println(a);
+    }
+}
